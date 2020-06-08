e929d8ea83256943b5dad6902f55f3a6f7f2ad1c
Sarah Rose <sarahrose63089@gmail.com>
2020-06-08 13:45:06 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,20 +8,11 @@ require "pry"
 # }
 
 def select_winner(passengers)
-#  winner = ""
-  #passengers.each do |suite, passenger|
-    #if suite == :suite_a && passenger.start_with?("A")
-      #passenger = winner
-    #end
-  #end
-  #winner
-#end
-
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
+  winner = ""
+  passengers.each do |suite, passenger|
+    if suite == :suite_a && passenger.start_with?("A")
+      winner = passenger
+    end
   end
-end
-winner
+  winner
 end
